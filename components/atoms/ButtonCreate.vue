365fdd841f5dc2ff25c32a9e738607a58d1b9e5e
Philippe Trudel <p.trudel1996@gmail.com>
2021-07-18 11:56:06 -0400

    create button link

---
+++ components/atoms/ButtonCreate.vue
@@ -0,0 +1,27 @@
+<template>
+  <nuxt-link to="/create" class="material-icons-outlined button-create">
+    add
+  </nuxt-link>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/var.scss";
+.button-create {
+  transition: all 0.2s ease-in-out;
+  color: #ffffff;
+  padding: 15px;
+  background-color: $purple;
+  border-radius: 100%;
+  position: fixed;
+  bottom: 5%;
+  right: 5%;
+  &:hover {
+    box-shadow: 4px 4px 0 #000;
+    transform: translate(-2px, -2px);
+  }
+}
+</style>
