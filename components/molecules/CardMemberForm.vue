bcc7842a1c0af61392dc49fc952c7da6483c0ef6
Philippe Trudel <p.trudel1996@gmail.com>
2021-07-18 11:57:03 -0400

    member step

---
+++ components/molecules/CardMemberForm.vue
@@ -0,0 +1,115 @@
+<template>
+  <button
+    :class="{ enabled: enabled }"
+    class="card-member-form"
+    @click="selectMember"
+  >
+    <div
+      :class="[
+        enabled ? 'material-icons-outlined enabled' : '',
+        'card-member-form__initals',
+      ]"
+    >
+      {{ initials }}
+    </div>
+    <div class="card-member-form__info">
+      <span class="card-member-form__name">{{ name }}</span>
+      <span class="card-member-form__email">{{ email }}</span>
+    </div>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    enabled: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    initials () {
+      if (this.enabled) {
+        return 'done_outline'
+      }
+      const initials = this.name.split(' ', 2)
+      if (initials.length < 2) {
+        return initials[0][0]
+      }
+      return `${initials[0][0]}${initials[1][0]}`
+    }
+  },
+  methods: {
+    selectMember () {
+      this.$emit('toggle', this.email)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/var.scss";
+
+.card-member-form {
+  width: 100%;
+  transition: all 0.2s ease-in-out;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-shrink: 0;
+  border: 1px solid;
+  border-color: #ffffff;
+  border-radius: 8px;
+  padding: 0.5em 1em;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 8px 8px 0 #000;
+    border: 1px solid #f0f0f0;
+    transform: translate(-4px, -4px);
+    &.enabled {
+      box-shadow: 8px 8px 0 $purple;
+    }
+  }
+  & > .card-member-form__initals {
+    transition: all 0.2s ease-in-out;
+    border-radius: 100%;
+    color: #fff;
+    background-color: #000;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 10px;
+    flex-shrink: 0;
+    &.enabled {
+      background-color: $purple;
+    }
+  }
+  & > .card-member-form__info {
+    display: flex;
+    flex-direction: column;
+    flex-shrink: 0;
+    justify-content: center;
+    align-items: flex-start;
+    & > .card-member-form__name {
+      text-transform: capitalize;
+      font-weight: 600;
+      font-size: 1rem;
+      color: $black;
+    }
+    & > .card-member-form__email {
+      color: $gray-dark;
+      font-size: 0.75rem;
+    }
+  }
+}
+</style>
--- components/organims/StepTeamMember.vue
+++ components/organims/StepTeamMember.vue
@@ -1,15 +1,129 @@
 <template>
-  <div>
-    Step team member
+  <div class="team form-layout">
+    <v-header-form title="Team Members" />
+    <div class="team__roles">
+      <div class="">
+        <button
+          v-for="(teamGroup, role) in team"
+          :key="role"
+          @click="setActiveRole(role)"
+        >
+          <h3 class="team__role" :class="{enabled : role === activeRole}">
+            {{ role }}
+          </h3>
+        </button>
+      </div>
+      <ul class="team__members">
+        <li
+          v-for="member in currentRoleMembers"
+          :key="member.email"
+          class="team__member"
+        >
+          <v-card-member-form
+            :name="member.name"
+            :email="member.email"
+            :enabled="isEnabled(member.email)"
+            @toggle="toggleCard"
+          />
+        </li>
+      </ul>
+    </div>
+    <v-button-form @click.native="nextStep" />
   </div>
 </template>
 
 <script>
-export default {
+import CardMemberForm from '~/components/molecules/CardMemberForm.vue'
+import HeaderForm from '~/components/molecules/Header.vue'
+import ButtonForm from '~/components/atoms/ButtonForm.vue'
+import teamMembers from '~/team.json'
 
+export default {
+  components: {
+    'v-card-member-form': CardMemberForm,
+    'v-button-form': ButtonForm,
+    'v-header-form': HeaderForm
+  },
+  props: {
+    value: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      team: {},
+      activeRole: null
+    }
+  },
+  computed: {
+    currentRoleMembers () {
+      return this.team[this.activeRole]
+    }
+  },
+  created () {
+    teamMembers.forEach((member) => {
+      if (!(member.role in this.team)) {
+        this.team[member.role] = []
+      }
+      this.team[member.role].push(member)
+    })
+    this.activeRole = Object.keys(this.team)[0]
+  },
+  methods: {
+    isEnabled (email) {
+      return this.value.includes(email)
+    },
+    setActiveRole (role) {
+      this.activeRole = role
+    },
+    nextStep () {
+      this.$emit('input', this.value)
+      this.$emit('nextStep')
+    },
+    toggleCard (email) {
+      if (this.value.includes(email)) {
+        this.$emit(
+          'input',
+          this.value.filter(i => i !== email)
+        )
+      } else {
+        this.$emit('input', [...this.value, email])
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "~/assets/scss/var.scss";
 
+.team {
+  & .team__roles {
+    & button {
+      margin-right: 20px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+    & .team__role {
+      text-transform: uppercase;
+      font-size: 1.15rem;
+      font-weight: 600;
+      &.enabled{
+        color: $purple;
+      }
+    }
+  }
+  & .team__members {
+    display: grid;
+    grid-template-columns: 20rem 20rem 20rem 20rem;
+    grid-row-gap: 40px;
+    grid-column-gap: 40px;
+
+    // & > .team__member {
+
+    // }
+  }
+}
 </style>
