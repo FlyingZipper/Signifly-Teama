19438d124107975e3c58958398926f4f7109dd75
Philippe Trudel <p.trudel1996@gmail.com>
2021-07-18 11:56:12 -0400

    form button style

---
+++ components/atoms/ButtonForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="button__form">
+    {{ name }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: 'Continue'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/var.scss";
+
+.button__form{
+  transition: all 0.2s ease-in-out;
+  text-transform: capitalize;
+  background-color: $purple;
+  font-weight: 600;
+  color: #FFFFFF;
+  font-size: 1rem;
+  padding: 0.5em 1em;
+  border-radius: 4px;
+  &:hover {
+    box-shadow: 4px 4px 0 #000;
+    transform: translate(-2px, -2px);
+  }
+}
+
+</style>
